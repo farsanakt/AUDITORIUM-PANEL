d709fcb078273e87d47930e98dbcd3f76479533b
Farsana kt <ffarsanakt@gmail.com>
2025-04-18 12:54:31 +0530

    added vercel.json file

--- src/pages/routes/auditoriumRoute.tsx
+++ src/pages/routes/auditoriumRoute.tsx
@@ -1,5 +1,5 @@
 import { Route,Routes } from "react-router-dom";
-import React from 'react'
+
 import LoginPage from "../auditorium/Login";
 import SignupPage from "../auditorium/Signup";
 
@@ -7,7 +7,7 @@ function AuditoriumRoute() {
   return (
     <div>
       <Routes>
-        <Route path='/login' element={<LoginPage/>} />
+        <Route path='/' element={<LoginPage/>} />
         <Route path='/signup' element={<SignupPage/>} />
       </Routes>
     </div>
